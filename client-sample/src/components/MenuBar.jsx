a619b2cd72cf623ab244197b26866d3810bd6844
pratikgore19 <pratikgore986@gmail.com>
2024-05-18 20:32:39 +0530

    removed linkContainer

--- client-sample/src/components/MenuBar.jsx
+++ client-sample/src/components/MenuBar.jsx
@@ -1,29 +1,21 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 const MenuBar = () => {
     return (
-        <Navbar className='mb-3' bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="/">MyWebsite</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <LinkContainer to="/">
-                        <Nav.Link>Home</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/all-users">
-                        <Nav.Link>All Users</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/create-user">
-                        <Nav.Link>Create User</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/about-us">
-                        <Nav.Link>About Us</Nav.Link>
-                    </LinkContainer>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <Navbar className="mb-3" bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/">MyWebsite</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/all-users">All Users</Nav.Link>
+          <Nav.Link as={Link} to="/create-user">Create User</Nav.Link>
+          <Nav.Link as={Link} to="/about-us">About Us</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     )
 }
 
