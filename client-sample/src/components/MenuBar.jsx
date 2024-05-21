65f06de8fb931c21400420887aee17c6467fab17
pratikgore19 <pratikgore986@gmail.com>
2024-05-21 21:19:00 +0530

    added menubar functionality to have more options

--- client-sample/src/components/MenuBar.jsx
+++ client-sample/src/components/MenuBar.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const MenuBar = () => {
-    return (
+    const links = [
+        { path: '/', label: 'Home' },
+        { path: '/all-users', label: 'All Users' },
+        { path: '/create-user', label: 'Create User' },
+        { path: '/about-us', label: 'About Us' }
+      ];
+    
+      const maxVisibleLinks = 5;
+    
+      return (
         <Navbar className="p-2" bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand as={Link} to="/">MyWebsite</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/all-users">All Users</Nav.Link>
-                    <Nav.Link as={Link} to="/create-user">Create User</Nav.Link>
-                    <Nav.Link as={Link} to="/about-us">About Us</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+          <Navbar.Brand as={Link} to="/">MyWebsite</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              {links.slice(0, maxVisibleLinks).map((link, index) => (
+                <Nav.Link key={index} as={Link} to={link.path}>{link.label}</Nav.Link>
+              ))}
+              {links.length > maxVisibleLinks && (
+                <Dropdown>
+                  <Dropdown.Toggle variant="dark">
+                    More
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {links.slice(maxVisibleLinks).map((link, index) => (
+                      <Dropdown.Item key={index + maxVisibleLinks} as={Link} to={link.path}>
+                        {link.label}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
-    )
+      );
 }
 
 export default MenuBar
